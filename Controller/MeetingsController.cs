43a94c760f3723e9182c9561ab21339aa1af9c4a
dalia <dalia01122004@gmail.com>
2025-04-20 23:08:58 +0200

    Add Controller folder

---
+++ Controller/MeetingsController.cs
@@ -0,0 +1,93 @@
+﻿using IEEE.Data;
+using IEEE.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IEEE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeetingsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+
+        public MeetingsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Meetings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<meetings>>> GetMeetings()
+        {
+            return await _context.meetings
+                .Include(m => m.Creator)
+                .Include(m => m.Committee)
+                .ToListAsync();
+        }
+
+        // GET: api/Meetings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<meetings>> GetMeeting(int id)
+        {
+            var meeting = await _context.meetings
+                .Include(m => m.Creator)
+                .Include(m => m.Committee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (meeting == null)
+                return NotFound();
+
+            return meeting;
+        }
+
+        // POST: api/Meetings
+        [HttpPost]
+        public async Task<ActionResult<meetings>> PostMeeting(meetings meeting)
+        {
+            _context.meetings.Add(meeting);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMeeting), new { id = meeting.Id }, meeting);
+        }
+
+        // PUT: api/Meetings/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMeeting(int id, meetings meeting)
+        {
+            if (id != meeting.Id)
+                return BadRequest();
+
+            _context.Entry(meeting).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.meetings.Any(m => m.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Meetings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMeeting(int id)
+        {
+            var meeting = await _context.meetings.FindAsync(id);
+            if (meeting == null)
+                return NotFound();
+
+            _context.meetings.Remove(meeting);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
